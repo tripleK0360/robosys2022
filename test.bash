416d2f6456ffabb9fce3c19ce04a322d22af3308
Koki Ikeda <s21c1010gm@s.chibakoudai.jp>
2022-10-27 12:30:41 +0900

    change test

--- test.bash
+++ test.bash
@@ -1,2 +1,16 @@
 #!/bin/bash
-seq 5 | ./plus
+# SPDX-FileCopyrightText: 2022 Koki Ikeda
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
