27761aa2a01bd3938fff390eb8fdefd8facaeaac
Koki Ikeda <s21c1010gm@s.chibakoudai.jp>
2022-10-27 12:57:27 +0900

    yamada deleted

--- test.bash
+++ test.bash
@@ -10,7 +10,15 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]     || ng ${LINENO}
+[ "${out}" = ""] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]     || ng ${LINENO}
+[ "${out}" = ""] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
