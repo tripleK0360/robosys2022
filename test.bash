52534f556d6ac651737b0c7e3bb4ff44fbb53182
Koki Ikeda <s21c1010gm@s.chibakoudai.jp>
2022-11-03 12:15:29 +0900

    change script

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Koki Ikeda
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -13,12 +13,12 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]     || ng ${LINENO}
-[ "${out}" = ""] || ng ${LINENO}
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]     || ng ${LINENO}
-[ "${out}" = ""] || ng ${LINENO}
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
